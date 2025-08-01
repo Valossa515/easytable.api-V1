4afbd7add6fed61fc0e1c49fea141c55702f7727
Valossa515 <fe.mmo515@gmail.com>
2025-08-01 18:49:22 -0300

    Criando Coxinhahandler e websocket

--- src/main/java/br/com/aftersunrise/easytable/services/KafkaPedidoConsumerService.java
+++ src/main/java/br/com/aftersunrise/easytable/services/KafkaPedidoConsumerService.java
@@ -6,33 +6,38 @@ import br.com.aftersunrise.easytable.shared.enums.PedidoStatus;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class KafkaPedidoConsumerService {
 
     private final ObjectMapper objectMapper;
     private final PedidoRepository pedidoRepository;
     private final RedisService redisService;
+    private final PedidoWebSocketPublisher webSocketPublisher;
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaPedidoConsumerService.class);
 
     @KafkaListener(topics = "${app.kafka.topic.pedido-criado}", groupId = "cozinha")
     public void consumirPedido(String mensagem) {
-        try{
+        try {
             Pedido pedido = objectMapper.readValue(mensagem, Pedido.class);
-            log.info("Pedido recebido na cozinha: {}", pedido.getId());
+            logger.info("Pedido recebido na cozinha: {}", pedido.getId());
 
             pedido.setStatus(PedidoStatus.EM_PREPARACAO);
             pedidoRepository.save(pedido);
 
             redisService.salvar("pedido:" + pedido.getId(), pedido, 60);
-        }
-        catch (Exception e) {
-            log.error("Erro ao processar pedido do Kafka", e);
+
+            webSocketPublisher.enviarParaCozinha(pedido); // notifica cozinha
+
+        } catch (Exception e) {
+            logger.error("Erro ao processar pedido do Kafka", e);
             throw new RuntimeException("Erro ao processar pedido", e);
         }
     }
-
 }
