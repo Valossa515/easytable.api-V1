7652e0b038fc0f7d7c44ada79b3711cda5b44389
Felipe Martins <fe.mmo515@gmail.com>
2026-01-30 09:58:43 -0300

    Fix statemachine factory configuration

---
+++ src/main/java/br/com/aftersunrise/easytable/services/PedidoStateMachineService.java
@@ -0,0 +1,34 @@
+package br.com.aftersunrise.easytable.services;
+
+import br.com.aftersunrise.easytable.shared.enums.PedidoStatus;
+import br.com.aftersunrise.easytable.shared.enums.PedidoStatusEvent;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoStateMachineService {
+
+    private final StateMachineFactory<PedidoStatus, PedidoStatusEvent> stateMachineFactory;
+
+    public PedidoStateMachineService(StateMachineFactory<PedidoStatus, PedidoStatusEvent> stateMachineFactory) {
+        this.stateMachineFactory = stateMachineFactory;
+    }
+
+    public PedidoStatus validarTransicao(PedidoStatus statusAtual, PedidoStatusEvent evento) {
+        StateMachine<PedidoStatus, PedidoStatusEvent> stateMachine = stateMachineFactory.getStateMachine();
+        stateMachine.stop();
+        stateMachine.getStateMachineAccessor()
+                .doWithAllRegions(accessor -> accessor.resetStateMachine(
+                        new DefaultStateMachineContext<>(statusAtual, null, null, null)));
+        stateMachine.start();
+
+        boolean aceitou = stateMachine.sendEvent(evento);
+        if (!aceitou || stateMachine.getState() == null) {
+            return null;
+        }
+
+        return stateMachine.getState().getId();
+    }
+}
---
+++ src/main/java/br/com/aftersunrise/easytable/shared/enums/PedidoStatusEvent.java
@@ -0,0 +1,9 @@
+package br.com.aftersunrise.easytable.shared.enums;
+
+public enum PedidoStatusEvent {
+    INICIAR_PREPARO,
+    MARCAR_PRONTO,
+    ENTREGAR,
+    CONFIRMAR_PAGAMENTO,
+    CANCELAR;
+}
